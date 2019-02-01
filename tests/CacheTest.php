213242388a56bd2e38f546cad815d969834ebdaa
xiehuanjin <xiehuanjin@globalegrow.com>
2019-02-01 14:59:45 +0800

    单元测试

---
+++ tests/CacheTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace lingyin\predis;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * 缓存单元测试
+ * 
+ * vendor/bin/phpunit --bootstrap vendor/autoload.php tests/CacheTest.php
+ * 
+ * @author yumancang
+ *
+ */
+class CacheTest extends TestCase
+{
+    
+    private $cache;
+    
+    public function setUp()
+    {
+
+    }
+
+}
---
+++ tests/ConnectionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace lingyin\predis;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * 缓存单元测试
+ * 
+ * vendor/bin/phpunit --bootstrap vendor/autoload.php tests/ConnectionTest.php
+ * 
+ * @author yumancang
+ *
+ */
+class ConnectionTest extends TestCase
+{
+    
+    private $cache;
+    
+    public function setUp()
+    {
+
+    }
+
+}
